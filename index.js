9fd83d6720a576da21dd8616b962cf97ceaf5ef9
Murat Yıldız <3416658-mrtyldz@users.noreply.gitlab.com>
2021-03-21 18:38:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ client.on('ready', () => {
 client.request('SET_ACTIVITY', {
 pid: process.pid,
 activity : {
-details : "Sende Katıl!", // Resmin yanında yazacak mesaj
+details : "text1",
 assets : {
-large_image : "ymy_logo", // Dev portaldaki resmin ismi
-large_text : "Sende Katıl!" // Resime fare imleci götürüldüğünde gözükecek yazı
+large_image : "image",
+large_text : "text2"
 },
-buttons : [{label : "Discord'a Katıl" , url : "https://discord.gg/KazHgb2"},{label : "Sitemize Gel!",url : "https://yazilimcilarinmolayeri.com"}]
+buttons : [{label : "Buton1" , url : "link1"},{label : "Buton2",url : "link2"}]
 }
 })
 })
-client.login({ clientId : "823237644326404137" }).catch(console.error);
+client.login({ clientId : "clientid" }).catch(console.error);
